924a841e188bae9696949e0aee08d87d6491d55b
Laurens Miers <laurensmiers@protonmail.com>
2025-03-30 19:54:30 +0200

    refactor(robot_results_parser): Update suite passed/failed statistics API
    RobotFramework(RF) v4 changed the suite.statistics API

--- dbbot/reader/robot_results_parser.py
+++ dbbot/reader/robot_results_parser.py
@@ -126,8 +126,8 @@ class RobotResultsParser(object):
         self._db.insert_or_ignore('suite_status', {
             'test_run_id': test_run_id,
             'suite_id': suite_id,
-            'passed': suite.statistics.all.passed,
-            'failed': suite.statistics.all.failed,
+            'passed': suite.statistics.passed,
+            'failed': suite.statistics.failed,
             'elapsed': suite.elapsed_time,
             'status': suite.status
         })
